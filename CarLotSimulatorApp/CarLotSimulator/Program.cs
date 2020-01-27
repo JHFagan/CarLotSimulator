541657c3fd6245d42c63d4db2248475065dda4ac
Jack Fagan <jhfagan579@gmail.com>
2020-01-27 09:56:12 -0600

    2nd bonus excercise

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,6 +6,8 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+
+            var lot1 = new CarLot();
             //TODO
 
             //Create a seperate class file called Car
@@ -15,8 +17,44 @@ namespace CarLotSimulator
 
 
             //Now that the Car class is created we can instanciate 3 new cars
+            var car1 = new Car();
+            var car2 = new Car(1204, "Ancient Honda", "Chariot", "*whinneing*", "*Yelling Romanly*", true);
+            var car3 = new Car()
+            {
+                Year = 2004 ,
+            Make = "Honda",
+            Model = "CRV",
+            EngineNoise = "vwooosh",
+            HonkNoise = "Honka-Honka",
+            IsDriveable = false
+            };
+
             //Set the properties for each of the cars
+            car1.Year =2001;
+            car1.Make ="GM" ;
+            car1.Model ="Oldsmobile";
+            car1.EngineNoise = "chugachugachuga" ;
+            car1.HonkNoise = "beep-beep bitch" ;
+            car1.IsDriveable = true;
+
             //Call each of the methods for each car
+            car1.MakeEngineNoise("vroooooooom");
+            car1.MakeHonkNoise("beeeeeeep");
+            car2.MakeEngineNoise();
+            car2.MakeHonkNoise();
+            car3.MakeEngineNoise();
+            car3.MakeHonkNoise();
+
+
+            lot1.Lot.Add(car1);
+            lot1.Lot.Add(car2);
+            lot1.Lot.Add(car3);
+
+            foreach (var car in lot1.Lot)
+            {
+                Console.WriteLine($"Make: {car.Make}, Model: {car.Model}, Year: {car.Year}");
+            }
+
 
             //*************BONUS*************//
 
